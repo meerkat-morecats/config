778faa1b950b1985f66e90c6c36de4d36c9bbb4a
kaik <kakcool918@gmail.com>
2018-12-29 15:50:39 +0800

    mysql

--- mysql/run.sh
+++ mysql/run.sh
@@ -2,4 +2,4 @@ mkdir conf data logs
 docker build . -t $1
 docker stop mymysql
 docker rm mymysql
-docker run -p 3306:3306 --name mymysql -v ~/development/mysql/conf:/etc/mysql/conf.d -v ~/development/mysql/logs:/logs -v ~/development/mysql/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:$1
+docker run -p 3306:3306 --name mymysql -v ~/mysql_config/conf:/etc/mysql/conf.d -v ~/mysql_config/logs:/logs -v ~/mysql_config/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:$1
