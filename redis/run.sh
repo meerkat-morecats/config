94cb394d6494d5216154fa381215065955cdfcf4
kangkai <kakcool918@gmail.com>
2019-09-12 19:58:01 +0800

    [feat]:修æ：:change redis

--- redis/run.sh
+++ redis/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 mkdir data
-docker build . -t myredis:$1
-docker stop myredis
-docker rm myredis
+docker build . -t
 docker run -p 6379:6379 -v $PWD/data:/data  -d redis:3.2 redis-server --appendonly yes
