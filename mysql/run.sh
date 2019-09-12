2cf4a8d68a37d4a1e0e1281449100c106ce57595
kangkai <kangkai@koolearn.com>
2019-09-12 16:54:49 +0800

    [feat]:增加redis

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+# 输入版本信息 一个参数
 mkdir conf data logs
-docker build . -t $1
+docker build . -t mymysql:$1
 docker stop mymysql
 docker rm mymysql
 docker run -p 3306:3306 --name mymysql -v ~/mysql_config/conf:/etc/mysql/conf.d -v ~/mysql_config/logs:/logs -v ~/mysql_config/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:$1 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
