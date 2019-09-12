8ea7788593f57a0ef55c5aac0923a74a0f8301b1
kangkai <kakcool918@gmail.com>
2019-09-12 17:59:22 +0800

    [feat]:unicode change

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 输入版本信息 一个参数
 mkdir conf data logs
-docker build . -t mysql_d1:$1
+docker build . -t mysql_dl:$1
 docker stop mysql_d1
 docker rm mysql_d1
-docker run -p 3306:3306 --name mysql_d1 -v ~/mysql_config/conf:/etc/mysql/conf.d -v ~/mysql_config/logs:/logs -v ~/mysql_config/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:$1 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run -p 3306:3306 --name mysql_d1 -v ~/mysql_config/conf:/etc/mysql/conf.d -v ~/mysql_config/logs:/logs -v ~/mysql_config/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=passwd -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
--- redis/run.sh
+++ redis/run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# 输入版本信息 一个参数
 mkdir data
-chmod 777 data
 docker build . -t myredis:$1
 docker stop myredis
 docker rm myredis
 docker run -p 6379:6379 -v $PWD/data:/data  -d redis:3.2 redis-server --appendonly yes
-
