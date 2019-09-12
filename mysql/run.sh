2142d0faa2b26f10c0781cc5a0a10f1e8552d751
kangkai <kakcool918@gmail.com>
2019-09-12 17:08:15 +0800

    [feat]:修改修æ:change mysql run.sh

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,5 +1,5 @@
 mkdir conf data logs
-docker build . -t $1
+docker build . -t mysql_d1:$1
 docker stop mysql_d1
 docker rm mysql_d1
-docker run -p 3306:3306 --name mysql_d1 -v ~/mysql_config/conf:/etc/mysql/conf.d -v ~/mysql_config/logs:/logs -v ~/mysql_config/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:$1 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run -p 3306:3306 --name mysql_d1 -v ~/mysql_config/conf:/etc/mysql/conf.d -v ~/mysql_config/logs:/logs -v ~/mysql_config/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:$1 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
